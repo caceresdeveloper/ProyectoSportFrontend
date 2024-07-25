1957f010571b37902d7e4ebefd6ed9d0120fb19e
caceresdeveloper <diego.caceres02@uptc.edu.co>
2024-07-25 12:20:19 -0500

    actualizacion apirest

--- src/services/apiRest.js
+++ src/services/apiRest.js
@@ -1,1 +1,1 @@
-export const ApiUrl = "https://proyecto-sport-backend-git-main-diegos-projects-331f5ed5.vercel.app/";
+export const ApiUrl = "proyecto-sport-backend-br1dix9ll-diegos-projects-331f5ed5.vercel.app/";
